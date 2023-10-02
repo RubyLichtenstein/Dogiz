af3cb24ef215a1fdffaf028086ef951540cb5577
Ruby Lichtenstein <reuven.lichtenstein@gmail.com>
2023-10-02 23:22:26 +0300

    fix async result

--- ui/src/main/java/com/rubylichtenstein/ui/common/AsyncStateHandler.kt
+++ ui/src/main/java/com/rubylichtenstein/ui/common/AsyncStateHandler.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.rubylichtenstein.domain.common.AsyncResult
 
 @Composable
 fun <T> AsyncStateHandler(
